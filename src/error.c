d78b76741df4d7732fd81c7a127749ca1739cc19
tokochiz <tokohana.1003@gmail.com>
2024-03-20 23:19:09 +0900

    fix : 関数名を変換

--- src/error.c
+++ src/error.c
@@ -6,13 +6,13 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 16:51:17 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/03/02 16:52:28 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/03/20 21:26:36 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-void put_error(const char *msg)
+void put_error_and_exit(const char *msg)
 {
 	perror(msg);
 	exit(EXIT_FAILURE);
