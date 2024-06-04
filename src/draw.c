cf6d19d56e721dbb292653633b1694e540eb4202
tokochiz <tokohana.1003@gmail.com>
2024-06-04 23:17:55 +0900

    add  ウィンドウの真ん中に表示できない

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 20:58:00 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/05/28 22:04:37 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/06/04 23:12:35 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	calc_isometric(double *x, double *y, int z, t_data *data)
 	*x = (original_x - original_y) * cos(data->view.angle_x);
 	*y = (original_x + original_y) * sin(data->view.angle_y) - z;
 	// デバッグ用のログ出力
-    printf("Isometric: original_x=%f, original_y=%f, x=%f, y=%f, z=%d\n", original_x, original_y, *x, *y, z);
+	//printf("Isometric: original_x=%f, original_y=%f, x=%f, y=%f, z=%d\n",original_x, original_y, *x, *y, z);
 }
 
 void	ajust_point(t_data *data)
@@ -35,8 +35,13 @@ void	ajust_point(t_data *data)
 		* data->view.depth;
 	z1 = data->map.height_map[(int)data->point.y1][(int)data->point.x1]
 		* data->view.depth;
+	
 	if (data->point.p == 1)
 	{
+		data->point.x0 *= data->view.zoom;
+        data->point.y0 *= data->view.zoom;
+        data->point.x1 *= data->view.zoom;
+        data->point.y1 *= data->view.zoom;
 		calc_isometric(&data->point.x0, &data->point.y0, z0, data);
 		calc_isometric(&data->point.x1, &data->point.y1, z1, data);
 	}
@@ -44,36 +49,39 @@ void	ajust_point(t_data *data)
 	data->point.x1 += data->point.shift_x;
 	data->point.y0 += data->point.shift_y;
 	data->point.y1 += data->point.shift_y;
+	
+	// デバッグ用出力
+    printf("Shifted: x0=%f, y0=%f, x1=%f, y1=%f\n", data->point.x0, data->point.y0, data->point.x1, data->point.y1);
 }
 
 void	calc_line_steps(t_data *data)
 {
+	float	delta_x;
+	float	delta_y;
 	float	step_x;
 	float	step_y;
 	float	max_v;
 
 	ajust_point(data);
-	step_x = data->point.x1 - data->point.x0;
-	step_y = data->point.y1 - data->point.y0;
-	if (step_x < 0)
-		step_x = step_x * -1;
-	if (step_y < 0)
-		step_y = step_y * -1;
-	if (step_x < step_y)
-	{
-		max_v = step_y;
-	}
+	delta_x = data->point.x1 - data->point.x0;
+	delta_y = data->point.y1 - data->point.y0;
+	if (delta_x < 0)
+		delta_x = -delta_x;
+	if (delta_y < 0)
+		delta_y = -delta_y;
+	if (delta_x < delta_y)
+		max_v = delta_y;
 	else
-	{
-		max_v = step_x;
-	}
-	step_x /= max_v;
-	step_y /= max_v;
-	while (((int)data->point.x0 - data->point.x1) || ((int)data->point.y0
-			- data->point.y1))
+		max_v = delta_x;
+	step_x = (data->point.x1 - data->point.x0) / max_v;
+	step_y = (data->point.y1 - data->point.y0) / max_v;
+	printf("delta_x = %f, delta_y = %f, step_x = %f, step_y = %f\n", delta_x, delta_y, step_x, step_y);
+	while ((int)(data->point.x0 - data->point.x1) || ((int)(data->point.y0
+			- data->point.y1)))
 	{
 		mlx_pixel_put(data->mlx, data->win, data->point.x0, data->point.y0,
 			data->color);
+			printf("x0 = %f, y0 = %f\n", data->point.x0, data->point.y0); // デバッグ用出力
 		data->point.x0 += step_x;
 		data->point.y0 += step_y;
 	}
@@ -87,15 +95,15 @@ void	set_points(t_data *data, int x, int y, int direction)
 		data->point.x1 = x + 1;
 		data->point.y0 = y;
 		data->point.y1 = y;
-		if (direction == 1)
-		{
-			data->point.x0 = x;
-			data->point.x1 = x;
-			data->point.y0 = y;
-			data->point.y1 = y + 1;
-		}
-		calc_line_steps(data);
 	}
+	if (direction == 1)
+	{
+		data->point.x0 = x;
+		data->point.x1 = x;
+		data->point.y0 = y;
+		data->point.y1 = y + 1;
+	}
+	calc_line_steps(data);
 }
 void	draw(t_data *data)
 {
--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:54:09 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/05/28 22:02:13 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/06/04 23:13:06 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-
 #include <libc.h>
 
 // __attribute__((destructor))
@@ -27,26 +26,25 @@ void	check_argc(int argc)
 
 int	main(int argc, char *argv[])
 {
-	t_data data;
+	t_data	data;
 
-	check_argc(argc);	
+	check_argc(argc);
 	check_file(argv[1]);
 	parse_file(argv[1], &data);
-	
-	 data.point.shift_x = WIN_WIDTH / 2;
-	 data.point.shift_x = WIN_HEIGHT /2 - 500;
-	 //data.point.p = 1;
-	 //data.view.depth = 1;
-	 data.view.angle_x = 0.523599; // 30度
-	 data.view.angle_y = 0.523599; // 30度
-	
+	data.point.shift_x = WIN_WIDTH / 2;
+	data.point.shift_x = WIN_HEIGHT / 2;
+	data.point.p = 1;
+	data.view.depth = 1;
+	data.view.angle_x = 0.523599; // 30度
+	data.view.angle_y = 0.523599; // 30度
+	if (data.map.height < 100 && data.map.width < 100)
+		data.view.zoom = 10;
+	else
+		data.view.zoom = 3;
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "FdF");
-		
 	draw(&data);
-	
-	//mlx_key_hook();
+	// mlx_key_hook();
 	mlx_loop(data.mlx);
-
 	return (0);
 }
