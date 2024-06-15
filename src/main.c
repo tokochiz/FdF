a73adc2e4a79c16eb9181c6f119fc49b2367194d
tokochiz <tokohana.1003@gmail.com>
2024-06-15 15:48:03 +0900

    fix : offsetを等角変化後mapから計算する方法に変更

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:54:09 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/06/14 22:15:38 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/06/15 15:41:56 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 
 void	set_offset(t_data *data)
 {
-	data->view.offset_x = (WIN_WIDTH - (data->map.width * data->view.scale))
-		/ 2;
-	data->view.offset_y = (WIN_HEIGHT - (data->map.height * data->view.scale))
-		/ 2;
+	double	map_width;
+	double	map_height;
+
+	find_min_max_iso(data);
+	map_width = (data->map.max_x - data->map.min_x) * data->view.scale;
+	map_height = (data->map.max_y - data->map.min_y) * data->view.scale;
+	data->view.offset_x = (((WIN_WIDTH - map_width) / 2)
+			- (data->map.min_x * data->view.scale));
+	data->view.offset_y = (((WIN_HEIGHT - map_height) / 2)
+			- (data->map.min_y * data->view.scale));
 }
 
 void	initialize(t_data *data)
@@ -32,9 +38,6 @@ void	initialize(t_data *data)
 	data->view.offset_x = 0;
 	data->view.offset_y = 0;
 	data->view.scale = 1;
-	data->mlx.mlx = mlx_init();
-	data->mlx.win = mlx_new_window(data->mlx.mlx, WIN_WIDTH, WIN_HEIGHT, 
-		"**** FdF ****");
 }
 
 void	check_argc(int argc)
@@ -49,15 +52,14 @@ int	main(int argc, char *argv[])
 
 	check_argc(argc);
 	check_file(argv[1]);
-	parse_file(argv[1], &data);
 	initialize(&data);
 	init_image(&data);
+	parse_file(argv[1], &data);
 	set_scale(&data);
 	set_offset(&data);
 	draw(&data);
 	mlx_key_hook(data.mlx.win, press, &data);
 	mlx_hook(data.mlx.win, DESTROY_NOTIFY, 1L << 17, close_window, &data);
 	mlx_loop(data.mlx.mlx);
-	
 	return (0);
 }
